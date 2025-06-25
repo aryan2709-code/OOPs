e7b955336c956c1f8b653a3ee758232f1aa40d96
Aryan <bhardwaj7a@gmail.com>
2025-06-25 19:45:29 +0530

    Added more info related to function hiding

--- FunctionHiding2.cpp
+++ FunctionHiding2.cpp
@@ -27,9 +27,14 @@ int main()
     d.display(); // calls Drived::display()
 
     // d.display(10); // error : base::display(int) is hidden
+    d.Base::display(10); // Works fine 
 }
 
 // Working Of Function Hiding 
 // If a  derived class declares a function (even with different parameters) that has the same name as a function in the base class , then all overloaded
 // versions of that function in the base class become hidden in the derived class scope.
+
+// Important :
+// even without using the "using" keyword , you can still call the base class functions , but only through explicit scope resolution , i.e d.Base::display(10),
+// So , hiding doesn't mean they are deleted , it just means that the derived class scope won't see them automatically , you must qualify them with base class name
 
