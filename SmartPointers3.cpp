5e850286e0af38bb44bf91d8aefbcb70a9731b9e
Aryan <bhardwaj7a@gmail.com>
2025-06-26 18:40:47 +0530

    Refactored the code

--- SmartPointers3.cpp
+++ SmartPointers3.cpp
@@ -19,7 +19,8 @@ int main()
     shared_ptr<Test> ptr1 = make_shared<Test>();
     weak_ptr<Test> wp = ptr1; // Doesn't increase use_count
     cout << "Use count : "<<ptr1.use_count()<<endl;
-    if(auto spt = wp.lock())
+    auto spt = wp.lock();
+    if(spt)
     {
         cout << "Object is still alive.\n";
     }
