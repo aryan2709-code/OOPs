a1f4f579bd1ad2d9ea8da57e9b5f5bea165a2e8e
Aryan <bhardwaj7a@gmail.com>
2025-11-18 19:20:55 +0530

    Added comments for a better understanding

--- Explicit2.cpp
+++ Explicit2.cpp
@@ -9,14 +9,22 @@ private:
 public:
     explicit Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
 
-    bool operator==(const Complex& obj) const {
+    // Using const is necessary here, because in the if condition, we are doing if (c1 == (Complex)(3.0))
+    // Here, (Complex)(3.0) creates a temporary object, an object that lives only for that line of code
+    // In C++, a standard reference Complex& is not allowed to point to a temporary object
+    // The compiler thinks, You asked for a reference that allows modification but this object is about to vanish,so i won't let 
+    // you do it
+
+    // You need to promise the compiler that you are not willing to make any changes through this reference
+    // This can be done by adding the const keyword to the argument
+    bool operator==(const Complex& obj) {
         return (real == obj.real && imag == obj.imag);
     }
 };
 
 int main() {
     Complex c1(3.0, 0.0);
-    if (c1 == (Complex)(3.0)) // ✅ Explicit cast
+    if (c1 == (Complex)(3.0)) //Explicit cast
         cout << "Same" << endl;
     else
         cout << "Not same" << endl;
